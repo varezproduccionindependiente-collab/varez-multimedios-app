370ebbf02fc3399b96c42b90d5764a5aca96066f
varezproduccionindependiente-collab <varezproduccionindependiente@gmail.com>
2026-09-22 15:15:29 -0300

    Add PWA service worker

---
+++ free_local/static/service-worker.js
@@ -0,0 +1,25 @@
+const CACHE = "fm-informes-v1";
+const ASSETS = ["/", "/static/manifest.webmanifest", "/static/icon.svg"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request).then(r => r || caches.match("/"))
+    )
+  );
+});
